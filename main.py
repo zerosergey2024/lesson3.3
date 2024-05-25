cd07a76a92b65844f2412161a1ef00f15e68f474
zerosergey2024 <rosinka_ekt@mail.ru>
2024-05-25 16:10:36 +0300

    new code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 SCREEN_WIDTH=800
 SCREEN_HEIGHT=600
 screen = pygame.display.set_mode(SCREEN_WIDTH,SCREEN_HEIGHT)
-main.py
+
 pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/Иконка к игре.jpg")
 pygame.display.set_icon(icon)
