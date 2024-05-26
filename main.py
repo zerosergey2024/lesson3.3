cfb50a8aa21f21ee9cb8bc38f325ddccdaf69b06
zerosergey2024 <rosinka_ekt@mail.ru>
2024-05-26 15:20:19 +0300

    add new code2

--- main.py
+++ main.py
@@ -19,8 +19,13 @@ target_y = random.randint(0,SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-running = True
-shots_fired = 0
+font = pygame.font.Font(None, 36)
+hits = 0
+
+def show_hits():
+    text = font.render(f'Hits: {hits}', True, (0, 0, 0))
+    screen.blit(text, (10, 10))
+
 
 running = True
 shots_fired = 0
@@ -61,27 +66,6 @@ while running:
 pygame.quit()
 
 
-# while running and shots_fired < 5:
-    screen.fill(color)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and shots_fired < 5:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-                shots_fired += 1
-
-
-    target_x += 1  # Пример непрерывного движения мишени
-    if target_x > SCREEN_WIDTH:
-        target_x = 0
-
-    screen.blit(target_img, (target_x, target_y))
-    pygame.display.update()
-
-pygame.quit()
 
 
